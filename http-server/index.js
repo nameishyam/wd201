c12b9850d408caec4728114f081e0f213c7b0308
nameishyam <geddamgowtham04@outlook.com>
2024-10-21 21:41:28 +0530

    Add .gitignore file and update file system operations
    - Add .gitignore file to ignore package-lock.json, hello-world, myFiles, and cli-demo
    - Update file system operations in fs.js to create, read, update, rename, and delete files
    - Add home.html file with basic HTML structure
    - Update index.js to read home.html and create an HTTP server
    - Add project.html file for project listing
    - Update package.json with start and test scripts

---
+++ http-server/index.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require("fs");
+
+const myfunction = () => {
+  console.log("shyam gowtham");
+};
+
+fs.readFile("home.html", (err, home) => {
+  console.log(home.toString());
+});
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
+    .createServer((request, response) => {
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      response.write(home);
+      response.end();
+    })
+    .listen(3000);
+});
+
+myfunction();
