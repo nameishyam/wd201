8b157cfba64d2e5598a69b67813a66694d14247f
nameishyam <geddamgowtham04@outlook.com>
2024-11-16 10:39:28 +0530

    Add unit tests for todo list functionality and update test script to use Jest

---
+++ todo-tests/__tests__/todo.js
@@ -0,0 +1,22 @@
+const todoList = require("../todo");
+
+const { all, add, markAsComplete } = todoList();
+
+describe("todolist test suits", () => {
+  test("add todo item", () => {
+    expect(all.length).toBe(0);
+    add({
+      title: "Todo test",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(1);
+  });
+});
+describe("todolist test suits", () => {
+  test("mark todo item as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
