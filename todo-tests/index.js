236a9eccffe9a8e5bb1d94bf671e3ab74657dbe3
nameishyam <geddamgowtham04@outlook.com>
2024-11-18 20:38:06 +0530

    Filter completed todos in getAllTodos and handle empty results in index.js

--- todo-tests/index.js
+++ todo-tests/index.js
@@ -1,4 +1,5 @@
 const { connect } = require("./connectDB");
+const todoList = require("./todo");
 const Todo = require("./TodoModel");
 
 const createTodo = async () => {
@@ -27,12 +28,19 @@ const countItems = async () => {
 const getAllTodos = async () => {
   try {
     const items = await Todo.findAll({
+      where: {
+        completed: true,
+      },
       order: [[`id`, `DESC`]],
     });
     const todosList = await items
       .map((item) => item.displayableStrings())
       .join(`\n`);
-    console.log(todosList);
+    if (todoList.length === 0) {
+      console.log(`no results found`);
+    } else {
+      console.log(todosList);
+    }
   } catch (error) {
     console.log(error);
   }
