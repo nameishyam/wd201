357c314942ed36d1e4691f32e1a79049e089e13b
nameishyam <geddamgowtham04@outlook.com>
2024-11-19 21:32:04 +0530

    Add Sequelize setup and CRUD routes for todos in the Express app

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,8 +1,21 @@
+const { request, response } = require("express");
 const express = require("express");
 const app = express();
 
-app.get(`/`, (request, response) => {
-  response.send(`hello world`);
+app.get(`/todos`, (request, response) => {
+  console.log(`Todo list`);
+});
+
+app.post(`\todos`, (request, response) => {
+  console.log(`Todo created`, request.body);
+});
+
+app.put(`/todos/:id/markAsComplete`, (request, response) => {
+  console.log(`Todo marked as complete`, request.params.id);
+});
+
+app.delete(`/todos/:id`, (request, response) => {
+  console.log(`Todo deleted`, request.params.id);
 });
 
 app.listen(3000, () => {
