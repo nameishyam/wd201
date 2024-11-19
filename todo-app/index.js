fe6f572f9e86a5b6f40630b50109e3b9afe840ac
nameishyam <geddamgowtham04@outlook.com>
2024-11-19 19:23:52 +0530

    Initialize todo-app with Express server and package.json

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+
 app.get(`/`, (request, response) => {
   response.send(`hello world`);
 });
+
 app.listen(3000, () => {
   console.log(`listeneing to port 3000`);
 });
