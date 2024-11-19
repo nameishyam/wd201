2a153e5aa9e4e8c3da1b9e8e9c501c0ab14750f5
nameishyam <geddamgowtham04@outlook.com>
2024-11-19 10:28:38 +0530

    Refactor Todo model and tests to integrate Sequelize for database operations added tests for database operations

--- todo-database/__tests__/todo.js
+++ todo-database/__tests__/todo.js
@@ -1,29 +1,20 @@
+// __tests__/todo.js
 /* eslint-disable no-undef */
-const todoList = require("../todo");
+const db = require("../models");
 
-const { all, add, markAsComplete } = todoList();
-
-describe("todolist test suits", () => {
-  beforeAll(() => {
-    add({
-      title: "Todo test",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-  });
-  test("add todo item", () => {
-    const todoItemCount = all.length;
-    add({
-      title: "Todo test - 2",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-    expect(all.length).toBe(todoItemCount + 1);
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
   });
 
-  test("mark todo item as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date(),
+    });
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
   });
 });
--- todo-database/models/todo.js
+++ todo-database/models/todo.js
@@ -1,3 +1,4 @@
+// models/todo.js
 'use strict';
 const {
   Model
@@ -9,13 +10,20 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+     static async addTask(params) {
+      return  await Todo.create(params);
+    }
     static associate(models) {
       // define association here
+    }
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
     }
   }
   Todo.init({
     title: DataTypes.STRING,
-    dueDate: DataTypes.DATE,
+    dueDate: DataTypes.DATEONLY,
     completed: DataTypes.BOOLEAN
   }, {
     sequelize,
