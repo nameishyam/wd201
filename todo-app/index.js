c54648898be45781266a9171dfbb5347cd2c88b4
nameishyam <geddamgowtham04@outlook.com>
2024-11-19 23:12:36 +0530

    Add Todo model, migration, and CRUD operations in Express app

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,17 +1,40 @@
 const { request, response } = require("express");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const { Todo } = require("./models");
 
 app.get(`/todos`, (request, response) => {
   console.log(`Todo list`);
 });
 
-app.post(`\todos`, (request, response) => {
+app.post(`/todos`, async (request, response) => {
   console.log(`Todo created`, request.body);
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+      completed: false,
+    });
+    return response.status(201).json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json({ error: error.message });
+  }
 });
 
-app.put(`/todos/:id/markAsComplete`, (request, response) => {
+app.put(`/todos/:id/markAsComplete`, async (request, response) => {
   console.log(`Todo marked as complete`, request.params.id);
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    const updatedTodo = await todo.markAsCompleted();
+    return response.status(200).json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json({ error: error.message });
+  }
 });
 
 app.delete(`/todos/:id`, (request, response) => {
